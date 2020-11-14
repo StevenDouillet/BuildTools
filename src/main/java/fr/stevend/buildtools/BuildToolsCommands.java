d0a665a34f1d2909c89e842c53f0512cf9cdb5a4
DOUILLET <Steven.douillet@gmail.com>
2020-11-14 20:15:09 +0100

    WIP temp commit

--- src/main/java/fr/stevend/buildtools/BuildToolsCommands.java
+++ src/main/java/fr/stevend/buildtools/BuildToolsCommands.java
@@ -23,48 +23,154 @@ public class BuildToolsCommands implements CommandExecutor {
 
             Player player = (Player) sender;
 
-            Inventory inventory = Bukkit.createInventory(player, 9, ChatColor.LIGHT_PURPLE + "BuildTools");
+            Inventory inventory = Bukkit.createInventory(player, 54, ChatColor.LIGHT_PURPLE + "BuildTools");
 
-            ItemStack cut = new ItemStack(Material.SHEARS, 1);
-            ItemMeta cut_meta = cut.getItemMeta();
-            cut_meta.setDisplayName(ChatColor.RED + "Cut");
-            ArrayList<String> cut_lore = new ArrayList<String>() {{
-                add("This will cut your last world edit selection");
+            ItemStack expand = new ItemStack(Material.GREEN_GLAZED_TERRACOTTA, 1);
+            ItemMeta expand_meta = expand.getItemMeta();
+            expand_meta.setDisplayName(ChatColor.RED + "Expand");
+            ArrayList<String> expand_lore = new ArrayList<String>() {{
+                add("Expanding your selection");
             }};
-            cut_meta.setLore(cut_lore);
-            cut.setItemMeta(cut_meta);
+            expand_meta.setLore(expand_lore);
+            expand.setItemMeta(expand_meta);
 
-            ItemStack copy = new ItemStack(Material.YELLOW_DYE, 1);
-            ItemMeta copy_meta = copy.getItemMeta();
-            copy_meta.setDisplayName(ChatColor.YELLOW + "Copy");
-            ArrayList<String> copy_lore = new ArrayList<String>() {{
-                add("This will copy your last world edit selection");
+            /* PLAYER HEAD */
+            /*ItemStack expand = new ItemStack(Material.PLAYER_HEAD, 1);
+            ItemMeta expand_meta = expand.getItemMeta();
+            expand_meta.setDisplayName(ChatColor.GREEN + "Expand");
+            ArrayList<String> expand_lore = new ArrayList<String>() {{
+                add("Expanding your selection");
             }};
-            copy_meta.setLore(copy_lore);
-            copy.setItemMeta(copy_meta);
+            expand_meta.setLore(expand_lore);
+            expand.setItemMeta(expand_meta);*/
 
-            ItemStack paste = new ItemStack(Material.PURPLE_DYE, 1);
-            ItemMeta paste_meta = paste.getItemMeta();
-            paste_meta.setDisplayName(ChatColor.BLUE + "Paste");
-            ArrayList<String> paste_lore = new ArrayList<String>() {{
-                add("This will paste your last clipboard item");
-            }};
-            paste_meta.setLore(paste_lore);
-            paste.setItemMeta(paste_meta);
-
-            ItemStack undo = new ItemStack(Material.COBWEB, 1);
+            ItemStack undo = new ItemStack(Material.MAGENTA_GLAZED_TERRACOTTA, 1);
             ItemMeta undo_meta = undo.getItemMeta();
-            undo_meta.setDisplayName(ChatColor.GREEN + "Undo");
+            undo_meta.setDisplayName(ChatColor.RED + "Undo");
             ArrayList<String> undo_lore = new ArrayList<String>() {{
-                add("This will Undo your last world edit action");
+                add("Undo your last action");
             }};
             undo_meta.setLore(undo_lore);
             undo.setItemMeta(undo_meta);
 
-            inventory.setItem(1, cut);
-            inventory.setItem(3, copy);
-            inventory.setItem(5, paste);
-            inventory.setItem(7, undo);
+            ItemStack pos1 = new ItemStack(Material.GOLD_NUGGET, 1);
+            ItemMeta pos1_meta = pos1.getItemMeta();
+            pos1_meta.setDisplayName(ChatColor.YELLOW + "First position");
+            ArrayList<String> pos1_lore = new ArrayList<String>() {{
+                add("Set the first position of your selection");
+            }};
+            pos1_meta.setLore(pos1_lore);
+            pos1.setItemMeta(pos1_meta);
+
+            ItemStack pos2 = new ItemStack(Material.GHAST_TEAR, 1);
+            ItemMeta pos2_meta = pos2.getItemMeta();
+            pos2_meta.setDisplayName(ChatColor.YELLOW + "Second position");
+            ArrayList<String> pos2_lore = new ArrayList<String>() {{
+                add("Set the second position of your selection");
+            }};
+            pos2_meta.setLore(pos2_lore);
+            pos2.setItemMeta(pos2_meta);
+
+            ItemStack set = new ItemStack(Material.BEDROCK, 1);
+            ItemMeta set_meta = set.getItemMeta();
+            set_meta.setDisplayName(ChatColor.LIGHT_PURPLE + "Set");
+            ArrayList<String> set_lore = new ArrayList<String>() {{
+                add("Fill the last selection");
+            }};
+            set_meta.setLore(set_lore);
+            set.setItemMeta(set_meta);
+
+            ItemStack replace = new ItemStack(Material.STICKY_PISTON, 1);
+            ItemMeta replace_meta = replace.getItemMeta();
+            replace_meta.setDisplayName(ChatColor.LIGHT_PURPLE + "Replace");
+            ArrayList<String> replace_lore = new ArrayList<String>() {{
+                add("Replace selection content");
+            }};
+            replace_meta.setLore(replace_lore);
+            replace.setItemMeta(replace_meta);
+
+            ItemStack wall = new ItemStack(Material.COBBLESTONE_WALL, 1);
+            ItemMeta wall_meta = wall.getItemMeta();
+            wall_meta.setDisplayName(ChatColor.LIGHT_PURPLE + "Wall");
+            ArrayList<String> wall_lore = new ArrayList<String>() {{
+                add("Fill the last selection with empty content");
+            }};
+            wall_meta.setLore(wall_lore);
+            wall.setItemMeta(wall_meta);
+
+            ItemStack cut = new ItemStack(Material.SHEARS, 1);
+            ItemMeta cut_meta = cut.getItemMeta();
+            cut_meta.setDisplayName(ChatColor.GOLD + "Cut");
+            ArrayList<String> cut_lore = new ArrayList<String>() {{
+                add("Cut your last selection");
+            }};
+            cut_meta.setLore(cut_lore);
+            cut.setItemMeta(cut_meta);
+
+            ItemStack rotate = new ItemStack(Material.COMPASS, 1);
+            ItemMeta rotate_meta = rotate.getItemMeta();
+            rotate_meta.setDisplayName(ChatColor.GOLD + "Rotate");
+            ArrayList<String> rotate_lore = new ArrayList<String>() {{
+                add("Cut your last selection");
+            }};
+            rotate_meta.setLore(rotate_lore);
+            rotate.setItemMeta(rotate_meta);
+
+            ItemStack copy = new ItemStack(Material.BOOKSHELF, 1);
+            ItemMeta copy_meta = copy.getItemMeta();
+            copy_meta.setDisplayName(ChatColor.GOLD + "Copy");
+            ArrayList<String> copy_lore = new ArrayList<String>() {{
+                add("Copy your last selection");
+            }};
+            copy_meta.setLore(copy_lore);
+            copy.setItemMeta(copy_meta);
+
+            ItemStack fix = new ItemStack(Material.LEAD, 1);
+            ItemMeta fix_meta = fix.getItemMeta();
+            fix_meta.setDisplayName(ChatColor.GOLD + "Fix");
+            ArrayList<String> fix_lore = new ArrayList<String>() {{
+                add("Fix your selection (water, lava or light)");
+            }};
+            fix_meta.setLore(fix_lore);
+            fix.setItemMeta(fix_meta);
+
+            ItemStack paste = new ItemStack(Material.KNOWLEDGE_BOOK, 1);
+            ItemMeta paste_meta = paste.getItemMeta();
+            paste_meta.setDisplayName(ChatColor.BLUE + "Paste");
+            ArrayList<String> paste_lore = new ArrayList<String>() {{
+                add("Paste your last clipboard item");
+            }};
+            paste_meta.setLore(paste_lore);
+            paste.setItemMeta(paste_meta);
+
+            ItemStack pasta = new ItemStack(Material.ENCHANTED_BOOK, 1);
+            ItemMeta pasta_meta = pasta.getItemMeta();
+            pasta_meta.setDisplayName(ChatColor.BLUE + "Paste -a");
+            ArrayList<String> pasta_lore = new ArrayList<String>() {{
+                add("Paste your last clipboard item without air");
+            }};
+            pasta_meta.setLore(pasta_lore);
+            pasta.setItemMeta(pasta_meta);
+
+            inventory.setItem(0, expand);
+            /*inventory.setItem(4, head);*/
+            inventory.setItem(8, undo);
+
+            inventory.setItem(12, pos1);
+            inventory.setItem(14, pos2);
+
+            inventory.setItem(20, set);
+            inventory.setItem(22, replace);
+            inventory.setItem(24, wall);
+
+            inventory.setItem(28, cut);
+            inventory.setItem(30, rotate);
+            inventory.setItem(32, copy);
+            inventory.setItem(34, fix);
+
+            inventory.setItem(39, paste);
+            inventory.setItem(41, pasta);
+
             player.openInventory(inventory);
         }
 
--- src/main/java/fr/stevend/buildtools/BuildToolsMenuHandler.java
+++ src/main/java/fr/stevend/buildtools/BuildToolsMenuHandler.java
@@ -1,12 +1,17 @@
 package fr.stevend.buildtools;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class BuildToolsMenuHandler implements Listener {
 
@@ -14,6 +19,7 @@ public class BuildToolsMenuHandler implements Listener {
     public void onPlayerUse(PlayerInteractEvent event){
         Player player = event.getPlayer();
 
+        if(!player.isOp()) {return;}
         if(player.getInventory().getItemInMainHand().getType() == Material.CARROT_ON_A_STICK){
             player.performCommand("bt");
         }
@@ -22,40 +28,191 @@ public class BuildToolsMenuHandler implements Listener {
     @EventHandler
     public void onMenuClick(InventoryClickEvent e) {
 
+        Player player = (Player) e.getWhoClicked();
+
         if(e.getView().getTitle().equalsIgnoreCase(ChatColor.LIGHT_PURPLE + "BuildTools")) {
 
-            if(e.getCurrentItem().getType() != null) {
+            e.setCancelled(true);
+            if(e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) {return;}
 
-                e.setCancelled(true);
-                Player player = (Player) e.getWhoClicked();
-                Material currentItem = e.getCurrentItem().getType();
+            switch(e.getCurrentItem().getType()) {
+                case GREEN_GLAZED_TERRACOTTA:
+                    player.closeInventory();
+                    player.performCommand("/expand vert");
+                    break;
 
-                switch(currentItem) {
-                    case SHEARS:
-                        player.performCommand("/cut");
-                        player.sendMessage(ChatColor.RED + "Cutting in progress ...");
-                        break;
+                case MAGENTA_GLAZED_TERRACOTTA:
+                    player.closeInventory();
+                    player.performCommand("/undo");
+                    break;
 
-                    case YELLOW_DYE:
-                        player.performCommand("/copy");
-                        player.sendMessage(ChatColor.YELLOW + "Copying in progress ...");
-                        break;
+                case GOLD_NUGGET:
+                    player.closeInventory();
+                    player.performCommand("/pos1");
+                    break;
 
-                    case PURPLE_DYE:
-                        player.performCommand("/paste");
-                        player.sendMessage(ChatColor.DARK_PURPLE + "Pasting in progress ...");
-                        break;
+                case GHAST_TEAR:
+                    player.closeInventory();
+                    player.performCommand("/pos2");
+                    break;
 
-                    case COBWEB:
-                        player.performCommand("/undo");
-                        player.sendMessage(ChatColor.GREEN + "Undoing in progress ...");
-                        break;
-                }
+                case SHEARS:
+                    player.closeInventory();
+                    player.performCommand("/cut");
+                    break;
 
-                player.closeInventory();
+                case BOOKSHELF:
+                    player.closeInventory();
+                    player.performCommand("/copy");
+                    break;
+
+                case KNOWLEDGE_BOOK:
+                    player.closeInventory();
+                    player.performCommand("/paste");
+                    break;
+
+                case LEAD:
+                    Inventory fixInv = Bukkit.createInventory(player, 27, ChatColor.LIGHT_PURPLE + "Choix du fix");
+                    ItemStack water = new ItemStack(Material.WATER_BUCKET, 1);
+                    ItemStack light = new ItemStack(Material.LANTERN, 1);
+                    ItemStack lava = new ItemStack(Material.LAVA_BUCKET, 1);
+                    fixInv.setItem(11, water);
+                    fixInv.setItem(13, light);
+                    fixInv.setItem(15, lava);
+
+                    player.closeInventory();
+                    player.openInventory(fixInv);
+                    break;
+
+                case COMPASS:
+                    Inventory rotateInv = Bukkit.createInventory(player, 27, ChatColor.LIGHT_PURPLE + "Choix de la rotation");
+                    ItemStack rotate90 = new ItemStack(Material.YELLOW_WOOL, 1);
+                    ItemStack rotate180 = new ItemStack(Material.ORANGE_WOOL, 1);
+                    rotateInv.setItem(12, rotate90);
+                    rotateInv.setItem(14, rotate180);
+
+                    player.closeInventory();
+                    player.openInventory(rotateInv);
+                    break;
+
+                case BEDROCK:
+                case COBBLESTONE_WALL:
+
+                    Inventory materialInv = Bukkit.createInventory(player, 54, ChatColor.LIGHT_PURPLE + "Choix du matériaux");
+
+                    ItemStack stone = new ItemStack(Material.STONE, 1);
+                    ItemStack cobblestone = new ItemStack(Material.COBBLESTONE, 1);
+                    ItemStack oak_planks = new ItemStack(Material.OAK_PLANKS, 1);
+                    ItemStack spruce_planks = new ItemStack(Material.SPRUCE_PLANKS, 1);
+                    ItemStack dark_oak_planks = new ItemStack(Material.DARK_OAK_PLANKS, 1);
+                    ItemStack beach = new ItemStack(Material.SPONGE, 1);
+                    ItemStack house_wall = new ItemStack(Material.CHISELED_STONE_BRICKS, 1);
+                    ItemStack medieval_road = new ItemStack(Material.RAIL, 1);
+
+                    materialInv.setItem(0, stone);
+                    materialInv.setItem(1, cobblestone);
+                    materialInv.setItem(2, oak_planks);
+                    materialInv.setItem(3, spruce_planks);
+                    materialInv.setItem(4, dark_oak_planks);
+                    materialInv.setItem(38, beach);
+                    materialInv.setItem(40, house_wall);
+                    materialInv.setItem(42, medieval_road);
+
+                    player.closeInventory();
+                    player.openInventory(materialInv);
+                    break;
 
             }
 
+        }
+
+        if(e.getView().getTitle().equalsIgnoreCase(ChatColor.LIGHT_PURPLE + "Choix du matériaux")) {
+
+            e.setCancelled(true);
+            if(e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) {return;}
+
+            switch(e.getCurrentItem().getType()) {
+                case STONE:
+                    player.closeInventory();
+                    player.performCommand("/set stone");
+                    break;
+
+                case COBBLESTONE:
+                    player.closeInventory();
+                    player.performCommand("/set cobblestone");
+                    break;
+
+                case OAK_PLANKS:
+                    player.closeInventory();
+                    player.performCommand("/set minecraft:oak_planks");
+                    break;
+
+                case SPRUCE_PLANKS:
+                    player.closeInventory();
+                    player.performCommand("/set minecraft:spruce_planks");
+                    break;
+
+                case DARK_OAK_PLANKS:
+                    player.closeInventory();
+                    player.performCommand("/set minecraft:dark_oak_planks");
+                    break;
+
+                case SPONGE:
+                    player.closeInventory();
+                    player.performCommand("/set 10%minecraft:dead_tube_coral_block,60%sand,5%minecraft:dead_horn_coral_block,1%minecraft:brain_coral_block,1%minecraft:tube_coral_block,1%minecraft:horn_coral_block,1%minecraft:bubble_coral_block,20%minecraft:gravel");
+                    break;
+
+                case CHISELED_STONE_BRICKS:
+                    player.closeInventory();
+                    player.performCommand("/set 6%minecraft:dead_bubble_coral_block,6%minecraft:dead_tube_coral_block,35%minecraft:stone,20%minecraft:cobblestone,16%minecraft:dead_brain_coral_block");
+                    break;
+
+                case RAIL:
+                    player.closeInventory();
+                    player.performCommand("/set 30%cobblestone,30%andesite,30%gravel");
+                    break;
+            }
+        }
+
+        if(e.getView().getTitle().equalsIgnoreCase(ChatColor.LIGHT_PURPLE + "Choix du fix")) {
+
+            e.setCancelled(true);
+            if(e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) {return;}
+
+            switch(e.getCurrentItem().getType()) {
+                case WATER_BUCKET:
+                    player.closeInventory();
+                    player.performCommand("/fixwater 100");
+                    break;
+
+                case LANTERN:
+                    player.closeInventory();
+                    player.performCommand("/fixlighting");
+                    break;
+
+                case LAVA_BUCKET:
+                    player.closeInventory();
+                    player.performCommand("/fixlava 100");
+                    break;
+            }
+        }
+
+        if(e.getView().getTitle().equalsIgnoreCase(ChatColor.LIGHT_PURPLE + "Choix de la rotation")) {
+
+            e.setCancelled(true);
+            if(e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) {return;}
+
+            switch(e.getCurrentItem().getType()) {
+                case YELLOW_WOOL:
+                    player.closeInventory();
+                    player.performCommand("/rotate 90");
+                    break;
+
+                case ORANGE_WOOL:
+                    player.closeInventory();
+                    player.performCommand("/rotate 180");
+                    break;
+            }
         }
 
     }
